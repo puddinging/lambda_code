6dee80cb0f8638fcab4ef08040ef5438a3c490f8
jiefeng <jiefengfire@163.com>
2020-05-10 21:43:49 +0800

    aaa

--- src/main/java/com/jiefeng/App.java
+++ src/main/java/com/jiefeng/App.java
@@ -1,13 +1,12 @@
 package com.jiefeng;
 
+import com.definesys.mpaas.query.util.MpaasQueryUtil;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(MpaasQueryUtil.encryptRowId(9, "1234567890ABCDEF"));
     }
 }
