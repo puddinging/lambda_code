f68c58ecc206cafae7f638fd231cc998622b1087
jiefeng <jiefengfire@163.com>
2020-05-11 18:33:41 +0800

    测试

--- src/main/java/com/jiefeng/App.java
+++ src/main/java/com/jiefeng/App.java
@@ -11,9 +11,20 @@ public class App {
     public static void main(String[] args) {
         FunctionTest<Integer, Integer> f = new FunctionTest<>();
         // 在此将方法作为返回值传入
-        for (int i = 0; i < 1000000; i++) {
-            System.out.println(f.functionTest(Sum::sum, 99999999));
+        // for (int i = 0; i < 1000000; i++) {
+        // System.out.println(f.functionTest(Sum::sum, 99999999));
+        // }
 
-        }
+        boolean b = f.functionTest4(t -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(System.currentTimeMillis());
+            }
+            if (t == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }, 1);
+        System.out.println(b);
     }
 }
