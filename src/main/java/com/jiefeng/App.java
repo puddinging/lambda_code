f66c41d0d3e3a85730acd255d6e9e59fee3ea60d
jiefeng <jiefengfire@163.com>
2020-05-16 12:15:47 +0800

    a

--- src/main/java/com/jiefeng/App.java
+++ src/main/java/com/jiefeng/App.java
@@ -1,5 +1,6 @@
 package com.jiefeng;
 
+import com.jiefeng.demo.Sum;
 import com.jiefeng.utils.FunctionTest;
 
 
@@ -11,9 +12,9 @@ public class App {
     public static void main(String[] args) {
         FunctionTest<Integer, Integer> f = new FunctionTest<>();
         // 在此将方法作为返回值传入
-        // for (int i = 0; i < 1000000; i++) {
-        // System.out.println(f.functionTest(Sum::sum, 99999999));
-        // }
+        for (int i = 0; i < 1000000; i++) {
+        System.out.println(f.functionTest(Sum::sum, 99999999));
+        }
 
         boolean b = f.functionTest4(t -> {
             for (int i = 0; i < 1000; i++) {
--- src/main/java/com/jiefeng/demo/Sum.java
+++ src/main/java/com/jiefeng/demo/Sum.java
@@ -3,7 +3,7 @@ package com.jiefeng.demo;
 public class Sum {
     /**
      * 在此实现自己的代码，需要和当作参数传入的接口方法参数返回值一致
-     * 
+     * 最重要就是需要参数和返回值一致，如果不一致就不可以   
      * @param i
      * @return
      */
@@ -13,5 +13,9 @@ public class Sum {
             x = j;
         }
         return Integer.valueOf(x);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1000));
     }
 }
